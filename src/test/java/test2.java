36a30aa17186f26828be7b178281940f385a0dbd
Vitalii Tymashkov <vitalii.tymashkov@gmail.com>
2022-10-23 19:44:52 +0200

    Updated test data creation code

--- src/test/java/test2.java
+++ src/test/java/test2.java
@@ -11,21 +11,23 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 // Input:  Web Page with table contains a task name, task creation date. Table has sorting on creation date
 // Task: Check if current test is implemented correctly and what will be the result of current run?
 public class test2 {
     private ElementsCollection taskListCollection = $$(By.xpath("//tr[@data xpath='table item']"));
     private SelenideElement sortAscLink = $(By.xpath("//th[@data xpath='tableHeadCell sort']"));
     private List<Task> listTasks = new ArrayList<>();
+    private TestPage testPage;
 
     @Test
-    public void testForms(){
+    public void testForms() {
+        List<Task> sortDesc = new ArrayList<>();
         List<Task> allTasksList = new LinkedList<>();
 
         taskListCollection.forEach(task -> {
-            allTasksList.add(new Task(task.$x("./*").getAttribute("name")));
-            allTasksList.add(new Task(task.$x("./*").getAttribute("endTime")));
+            allTasksList.add(new Task(
+                    task.$x("./*").getAttribute("name"),
+                    task.$x("./*").getAttribute("endTime")));
 
             List<Task> list1 = allTasksList;
             List<Task> list2 = allTasksList;
@@ -55,6 +57,16 @@ public class test2 {
 
         public String getEndTime() {
             return endTime;
+        }
+    }
+
+    private class TestPage {
+        public List<Task> sortAscending(List<Task> allTasksList) {
+            return new ArrayList<Task>();
+        }
+
+        public List<Task> sortDescending(List<Task> allTasksList) {
+            return new ArrayList<Task>();
         }
     }
 }
